b44255944d4ef8c342e57f280be652a1863b263b
ounie <ounie@abeaec7e-9478-5847-90f6-010a71e92303>
2016-04-26 08:18:55 +0000

    添加任务文件
    git-svn-id: https://192.168.56.1/svn/gsc3280-app-all@6 abeaec7e-9478-5847-90f6-010a71e92303

---
+++ app/task.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "can_gsc3280.h"
+#include "timer_gsc3280.h"
+#include <def.h>
+#include "canfestival.h"
+#include "OD_0_0.h"
+#include "task.h"
+
+
+void systick_task(void)
+{
+    
+}
+
+
